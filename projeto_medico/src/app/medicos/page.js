a96536354ea88acb4546e198fda46af28a92a88b
cappeLindo <vitorcappe@gmail.com>
2025-02-12 07:32:34 -0400

    criação da pasta medicos e alocamento da API

---
+++ projeto_medico/src/app/medicos/page.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import styles from './medico.module.css';
+ 
+export default function retornarMedicos() { 
+    return (
+        <div>
+            
+        </div>
+    );
+ }
